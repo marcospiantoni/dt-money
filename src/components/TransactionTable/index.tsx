169e08ebfe65395b58ab2e1096ee79671c8c30ec
Marcos <marcospiantoni360@gmail.com>
2023-02-20 14:56:33 -0300

    fake resquest api with MirageJs e Axios

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
+import { api } from "../../service/api";
 import { Container } from "./styles";
 
 export function TransactionTable() {
+ useEffect(() => {
+  api.get('/transactions')
+  .then(response => console.log(response.data))
+ }, []);
+
   return  (
    <Container>
     <table>
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createServer } from 'miragejs'
 import { App } from './App'
+
+createServer({
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 'deposit',
+          category: 'Food',
+          createdAd: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
