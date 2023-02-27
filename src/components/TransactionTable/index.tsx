d992ecb448d82ce9e82d466d5a7621fa3d879ce7
Marcos <marcospiantoni360@gmail.com>
2023-02-27 18:52:41 -0300

    button delete-styled

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,8 +1,14 @@
+import { Trash } from "phosphor-react";
+import CodeSimple from "phosphor-react/dist/icons/CodeSimple";
 import { useTransactions } from "../../hooks/useTransactions";
-import { Container } from "./styles";
+import { Container, DeleteTranslations } from "./styles";
 
 export function TransactionTable() {
  const { transactions } = useTransactions()
+
+ function DeleteTranslation() {
+  console.log('deletar')
+ }
 
   return  (
    <Container>
@@ -32,10 +38,14 @@ export function TransactionTable() {
              new Date(transaction.createdAt)
             )}
           </td>
+          <td>
+          <DeleteTranslations title="Deletar transação">
+              <Trash size={25} onClick={DeleteTranslation} />
+          </DeleteTranslations>
+          </td>
         </tr>
         )
       })
-
       }
       </tbody>
     </table>
--- src/components/TransactionTable/styles.ts
+++ src/components/TransactionTable/styles.ts
@@ -35,5 +35,19 @@ export const Container = styled.div`
     }
   }
  }
+`
 
+export const DeleteTranslations = styled.button`
+color: var(--text-body);
+background: transparent;
+border: 0;
+transition: 0.2s;
+
+cursor: pointer;
+
+&:hover {
+  color: var(--red);
+  padding: 5px;
+  transition: 0.5s;
+}
 `
